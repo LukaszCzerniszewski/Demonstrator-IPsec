fc4efc9f1c2a1495546e129263ca0a477d4baf8c
Celcik <lukasz.czerniszewski@gmail.com>
2022-06-15 19:16:56 -0400

    awf

---
+++ run.sh
@@ -0,0 +1,6 @@
+sudo apt install python3-pip
+pip install flask
+pip install pyDH
+pip install scapy
+python3 app.py
+
---
+++ wymagania.sh
@@ -0,0 +1,5 @@
+sudo apt install python3-pip
+pip install flask
+pip install pyDH
+pip install scapy
+
